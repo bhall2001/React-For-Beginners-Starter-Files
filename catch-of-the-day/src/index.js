807d66f655f71e2511345ac3c2fee4042eb9484b
Bob <bhall_2001@yahoo.com>
2018-02-13 15:48:12 -0500

    Completed Chapter 9

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
+import {BrowserRouter, Match, Miss} from 'react-router';
+
 import './css/style.css';
 import App from './components/app';
+import NotFound from "./components/notFound";
 
 import StorePicker from './components/StorePicker';
 
-render(<App/>, document.querySelector('#main'));
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={StorePicker} />
+                <Match exactly pattern="/store/:storeId" component={App} />
+                <Miss component={NotFound} />
+            </div>
+        </BrowserRouter>
+    )
+}
+
+render(<Root/>, document.querySelector('#main'));
