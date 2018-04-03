edfecfd8d6b175a3029dcaac4813c9f2b3358f2e
Bob Hall <bhall_2001@yahoo.com>
2018-04-03 18:58:32 -0400

    chapt 7

--- catch-of-the-day/src/components/inventory.js
+++ catch-of-the-day/src/components/inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Inventory extends React.Component {
     render () {
         return (
-            <p>Inventory</p>
+             <div className="inventory">Inventory!!!</div>
         )
     }
 }
